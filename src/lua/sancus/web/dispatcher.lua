6b6e70d5eab3e105b3b9edbabcb834a182e226ee
Alejandro Mery <amery@geeks.cl>
2012-05-01 17:42:36 +0200

    PathMapper: add wsapi path_info pattern based dispatcher

---
+++ src/lua/sancus/web/dispatcher.lua
@@ -0,0 +1,68 @@
+--
+
+local Class = assert(require"sancus.object").Class
+local URLTemplateCompiler = assert(require"sancus.web.urltemplate").URLTemplateCompiler
+
+local pp = require("sancus.utils").pprint
+
+local M = Class{
+	compile = URLTemplateCompiler
+}
+
+function M:__call(wsapi_env)
+	local h = self:find_handler(wsapi_env)
+
+	if h then
+		return h(wsapi_env)
+	else
+		return 404
+	end
+end
+
+function M:find_handler(env)
+	local script_name = env.headers["SCRIPT_NAME"] or ""
+	local path_info = env.headers["PATH_INFO"] or ""
+
+	for _, t in ipairs(self.patterns) do
+		local regex, h, kw = unpack(t)
+		local c, p = regex:match(path_info)
+
+		if p then
+			local matched_path_info = path_info:sub(1, p-1)
+			local extra_path_info = path_info:sub(p)
+
+			if #extra_path_info == 0 or
+				(#extra_path_info > 0 and extra_path_info:sub(1,1) == "/") then
+				-- good match
+
+				local routing_args = env.headers["sancus.routing_args"] or {}
+
+				-- import captures
+				for k,v in pairs(c) do
+					routing_args[k] = v
+				end
+				-- and import extra add() args
+				if kw then
+					for k,v in pairs(kw) do
+						routing_args[k] = v
+					end
+				end
+
+				env.headers["sancus.routing_args"] = routing_args
+				env.headers["SCRIPT_NAME"] = script_name .. matched_path_info
+				env.headers["PATH_INFO"] = extra_path_info
+
+				return h
+			end
+		end
+	end
+end
+
+function M:add(expr, h, kw)
+	p = {self.compile(expr), h, kw}
+	self.patterns[#self.patterns + 1] = p
+end
+
+return {
+	PathMapper = function () return M({patterns = {},}) end
+}
