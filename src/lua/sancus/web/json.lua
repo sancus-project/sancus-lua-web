0197cd806747676811797d2615965fb83eec3191
Alejandro Mery <amery@geeks.cl>
2013-04-01 21:57:42 +0200

    sancus.web.json: improved to use __tostring() and Array, Object and Null classes

--- src/lua/sancus/web/json.lua
+++ src/lua/sancus/web/json.lua
@@ -5,42 +5,27 @@
 --
 
 local type, tostring, pairs, ipairs = type, tostring, pairs, ipairs
+local select = select
+local setmetatable, getmetatable = setmetatable, getmetatable
 local tconcat = table.concat
 
 local _M = { _NAME = ... }
 setfenv(1, _M)
 
+-- String
+--
 local function quote(s)
 	-- TODO: replace with lpeg
 	return s:gsub("\\","\\\\"):gsub("\"", '\\"'):gsub("\n", "\\n"):gsub("\t","\\t")
 end
 
-function json_encode(v)
-	local t = type(v)
-	local s
-	if t == "nil" then
-		s = "null"
-	elseif t == "boolean" or t == "number" then
-		s = tostring(v)
-	elseif t == "string" then
-		s = json_encode_string(v)
-	elseif #v > 0 then
-		s = json_encode_array(v)
-	else
-		s = json_encode_object(v)
-		-- prefer empty arrays instead of empty objects
-		if s == "{}" then
-			s = "[]"
-		end
-	end
-	return s
-end
-
-function json_encode_string(v)
+local function json_encode_string(v)
 	return "\"" .. quote(v) .. "\""
 end
 
-function json_encode_array(o)
+-- Array
+--
+local function json_encode_array(o)
 	local t = {}
 	for _, v in ipairs(o) do
 		t[#t+1] = json_encode(v)
@@ -53,7 +38,33 @@ function json_encode_array(o)
 	end
 end
 
-function json_encode_object(o)
+local json_array_mt = {
+	__tostring = function(self)
+		return json_encode_array(self)
+	end,
+}
+
+function Array(...)
+	local l = select('#', ...)
+	local o
+
+	if l == 0 then
+		o = {}
+	elseif l > 1 then
+		o = { ... }
+	else
+		o = select(1, ...)
+		if type(o) ~= 'table' then
+			o = { o }
+		end
+	end
+	setmetatable(o, json_array_mt)
+	return o
+end
+
+-- Object
+--
+local function json_encode_object(o)
 	local t = {}
 	local fmt = "%s: %s"
 
@@ -67,6 +78,71 @@ function json_encode_object(o)
 	else
 		return "{}"
 	end
+end
+
+local json_object_mt = {
+	__tostring = function(self)
+		return json_encode_object(self)
+	end,
+}
+
+function Object(...)
+	local l = select('#', ...)
+	local o
+
+	if l == 0 then
+		o = {}
+	elseif l > 1 then
+		o = { ... }
+	else
+		o = select(1, ...)
+		if type(o) ~= 'table' then
+			o = { o }
+		end
+	end
+	setmetatable(o, json_object_mt)
+	return o
+end
+
+-- Null
+--
+local json_null_mt = {
+	__tostring = function(self)
+		return "null"
+	end,
+	__call = function(self)
+		return self
+	end,
+}
+Null = {}
+setmetatable(Null, json_null_mt)
+
+-- Generic Encoder
+--
+function json_encode(v)
+	local t = type(v)
+	local s
+	if t == "nil" then
+		s = "null"
+	elseif t == "boolean" or t == "number" then
+		s = tostring(v)
+	elseif t == "string" then
+		s = json_encode_string(v)
+	else
+		local mt = getmetatable(v)
+		if mt ~= nil and mt.__tostring ~= nil then
+			s = tostring(v)
+		elseif #v > 0 then
+			s = json_encode_array(v)
+		else
+			s = json_encode_object(v)
+			-- prefer empty arrays instead of empty objects
+			if s == "{}" then
+				s = "[]"
+			end
+		end
+	end
+	return s
 end
 
 return _M
