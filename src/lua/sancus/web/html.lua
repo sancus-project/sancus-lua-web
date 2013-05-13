f12f45a7137c7c47bdb6f8e73c2572e6e915ee1a
Alejandro Mery <amery@geeks.cl>
2013-05-13 17:19:19 +0200

    web.html: Raw: wrapping class for pre-escaped strings

--- src/lua/sancus/web/html.lua
+++ src/lua/sancus/web/html.lua
@@ -6,10 +6,13 @@
 
 local utils = require"sancus.utils"
 local lpeg = require"lpeg"
+local Class = require"sancus.object.Class"
 
 local type, tostring, ipairs = type, tostring, ipairs
 local getmetatable = getmetatable
 local tconcat = table.concat
+
+local error = utils.error
 
 local _M = { _NAME = ... }
 setfenv(1, _M)
@@ -26,7 +29,7 @@ do
 		['"'] = "&quot;",
 		["'"] = "&#39;",
 	}
-	local risky = lpeg.S"&<>'\""
+	local risky = lpeg.S[[&<>'"]]
 	local safe = lpeg.P(1) - risky
 
 	safe = lpeg.C(safe^1)
@@ -86,5 +89,42 @@ function encode(v)
 	end
 	return s
 end
+
+-- HTML friendly string
+--
+Raw = Class{
+	init = function(cls, s)
+		-- flatten
+		local t = type(s)
+		if t == "table" then
+			if #s == 1 then
+				s = s[1]
+				t = type(s)
+			else
+				if #s == 0 then
+					s = ""
+				else
+					s = tconcat(s, "")
+				end
+				t = "string"
+			end
+		end
+		if s == nil then
+			s = ""
+		elseif t == "number" or t == "boolean" then
+			s = tostring(s)
+		elseif t ~= "string" then
+			error("html.Raw:%s: Invalid data type", 1, t)
+		end
+
+		return { s }
+	end,
+	html_encoded = function(self)
+		return self[1] or ""
+	end,
+	__tostring = function(self)
+		return self:html_encoded()
+	end,
+}
 
 return _M
