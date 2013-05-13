a7cab452f5b9dd8936c1f0bc3e8948fc3a513d6b
Alejandro Mery <amery@geeks.cl>
2013-05-13 13:15:28 +0200

    html: Implement html_encoded_string()

--- src/lua/sancus/web/html.lua
+++ src/lua/sancus/web/html.lua
@@ -4,6 +4,9 @@
 -- Copyright (c) 2013, Alejandro Mery <amery@geeks.cl>
 --
 
+local utils = require"sancus.utils"
+local lpeg = require"lpeg"
+
 local type, tostring, ipairs = type, tostring, ipairs
 local getmetatable = getmetatable
 local tconcat = table.concat
@@ -11,8 +14,45 @@ local tconcat = table.concat
 local _M = { _NAME = ... }
 setfenv(1, _M)
 
+-- html escape strings
+--
+local html_escape
+
+do
+	local escaped = {
+		["&"] = "&amp;",
+		["<"] = "&lt;",
+		[">"] = "&gt;",
+		['"'] = "&quot;",
+		["'"] = "&#39;",
+	}
+	local risky = lpeg.S"&<>'\""
+	local safe = lpeg.P(1) - risky
+
+	safe = lpeg.C(safe^1)
+	risky = lpeg.C(risky)/escaped
+
+	html_escape = lpeg.Ct((safe + risky)^0) * -1
+end
+
 function html_encoded_string(v)
-	-- TODO: Implement ;-)
+	local t
+
+	if not v then
+		v = ""
+	elseif v~= "" then
+		t = html_escape:match(v)
+		if not t or #t == 0 then
+			utils.stderr_prefixed_lines("html_encoded_string",
+				utils.pformat(t, v))
+			v = ""
+		elseif #t == 1 then
+			v = t[1]
+		else
+			v = tconcat(t, "")
+		end
+	end
+
 	return v
 end
 
